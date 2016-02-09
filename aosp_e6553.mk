6741f4ec320bdd6194ea2f9ecc9fdb160037a61b
Alin Jerpelea <alin.jerpelea@sonymobile.com>
2016-02-09 20:00:17 +0100

    device rework
    We split device from aosp_device so that it can be included
    easy in custom projects.
    
    New projects should create a new target named <project>_device
    which contains project specific configuration and options.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sonymobile.com>

--- aosp_e6553.mk
+++ aosp_e6553.mk
@@ -14,53 +14,13 @@
 
 TARGET_KERNEL_CONFIG := aosp_kitakami_ivy_defconfig
 
-DEVICE_PACKAGE_OVERLAYS += \
-    device/sony/ivy/overlay
-
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product, device/sony/kitakami/platform.mk)
-$(call inherit-product, vendor/sony/ivy/ivy-vendor.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/sony/ivy/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-
-PRODUCT_COPY_FILES += \
-    device/sony/ivy/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    device/sony/ivy/rootdir/system/etc/BCM4356.hcd:system/etc/firmware/BCM43xx.hcd \
-    device/sony/ivy/rootdir/system/etc/wifi/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
-    device/sony/ivy/rootdir/system/etc/sensor_def_qcomdev.conf:system/etc/sensor_def_qcomdev.conf \
-    device/sony/ivy/rootdir/system/etc/thermanager.xml:system/etc/thermanager.xml \
-    device/sony/ivy/rootdir/system/etc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
-    device/sony/ivy/rootdir/system/etc/libnfc-nxp.conf:system/etc/libnfc-nxp.conf \
-    device/sony/ivy/rootdir/system/etc/mixer_paths.xml:system/etc/mixer_paths.xml
-
-# Device Init
-PRODUCT_PACKAGES += \
-    init.recovery.ivy \
-    init.ivy \
-    ueventd.ivy
-
-# Lights
-PRODUCT_PACKAGES += \
-    lights.ivy
-
-# Simple PowerHAL
-PRODUCT_PACKAGES += \
-    power.ivy
-
-# NFC config
-PRODUCT_PACKAGES += \
-    nfc_nci.ivy
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 PRODUCT_NAME := aosp_e6553
 PRODUCT_DEVICE := ivy
 PRODUCT_MODEL := Xperia Z3+ (AOSP)
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
-
-PRODUCT_AAPT_CONFIG := large
-PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=480 \
-    ro.usb.pid_suffix=1C9
