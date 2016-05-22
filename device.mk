c50f47d37595f549c386e96ce4d2e6c6f937fb26
David <Davidteri91@gmail.com>
2016-05-22 17:47:01 +0200

    ivy: Build SimToolKit
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- device.mk
+++ device.mk
@@ -50,6 +50,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     nfc_nci.ivy
 
+# SimToolKit
+PRODUCT_PACKAGES += \
+    Stk
+
 PRODUCT_AAPT_CONFIG := large
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
